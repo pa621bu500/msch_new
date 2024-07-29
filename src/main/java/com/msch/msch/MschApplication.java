f2db0500d76484c1c832f02fae22a05afb8aa4ae
pa621bu500 <pa621bu500@gmail.com>
2024-07-29 17:24:25 +0800

    include validations, add dev and deploy application properties

--- src/main/java/com/msch/msch/MschApplication.java
+++ src/main/java/com/msch/msch/MschApplication.java
@@ -7,17 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-
-import java.util.concurrent.ScheduledFuture;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class MschApplication  implements CommandLineRunner {
+@PropertySource("classpath:application-dev.properties")
+public class MschApplication implements CommandLineRunner {
 
-	private static final Logger logger = LoggerFactory.getLogger(ManualScheduler.class);
+	private static final Logger logger = LoggerFactory.getLogger(MschApplication.class);
+
 	@Value("${default.scheduler.interval}")
 	private long defaultSchedulerInterval;
-	@Value("${bat.file.path}")
+
 	private String batFilePath;
 	@Autowired
 	private ManualScheduler manualScheduler;
@@ -27,22 +27,57 @@ public class MschApplication  implements CommandLineRunner {
 		SpringApplication.run(MschApplication.class, args);
 	}
 
-	//takes in the interval in minutes from command line argument
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		if (args.length > 0) {
 			try {
-				intervalInMinutes = Long.parseLong(args[0]);
+				if (args.length == 1) {
+					batFilePath = args[0];
+					intervalInMinutes = defaultSchedulerInterval;
+				} else if (args.length == 2) {
+					if (isInteger(args[0])) {
+						intervalInMinutes = Long.parseLong(args[0]);
+						if (args[1].endsWith(".bat")) {
+							batFilePath = args[1];
+						} else {
+							logger.error("Invalid .bat file extension: {}", args[1]);
+							return;
+						}
+					} else if (isInteger(args[1])) {
+						intervalInMinutes = Long.parseLong(args[1]);
+						if (args[0].endsWith(".bat")) {
+							batFilePath = args[0];
+						} else {
+							logger.error("Invalid .bat file extension: {}", args[0]);
+							return;
+						}
+					} else {
+						logger.error("Invalid arguments. Expected .bat file path and interval in minutes.");
+						logger.info("e.g. arg1 = C:\\Users\\xx\\xx.bat, arg2 = 5");
+						logger.info("e.g. java -jar C:\\Users\\xx\\xx.jar " +
+								"C:\\Users\\xx\\xx.bat 2");
+						return;
+					}
+				} else {
+					logger.error("Invalid number of arguments. Expected 1 or 2 arguments.");
+					return;
+				}
 				logger.info("Scheduler interval set to {} minutes.", intervalInMinutes);
+				manualScheduler.startScheduler(intervalInMinutes, batFilePath);
 			} catch (NumberFormatException e) {
-				logger.error("Invalid interval value provided. Using default interval of "+ defaultSchedulerInterval +" minutes.");
-				intervalInMinutes = defaultSchedulerInterval;
+				logger.error("Invalid number format for interval: {}", e.getMessage());
 			}
 		} else {
-			intervalInMinutes = defaultSchedulerInterval;
-			logger.info("Scheduler interval set to {} minutes.", intervalInMinutes);
+			logger.error("No arguments provided. Expected .bat file path and interval in minutes (optional).");
 		}
-		manualScheduler.startScheduler(intervalInMinutes,batFilePath);
 	}
 
+	private boolean isInteger(String str) {
+		try {
+			Long.parseLong(str); // Use Long instead of Integer to match the type of intervalInMinutes
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
 }
